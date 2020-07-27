56fac2ff1cbde80f220d49bb4c4497ae2938c828
majiameng <666@.com>
2020-07-27 11:51:32 +0800

    update file

--- src/File.php
+++ src/File.php
@@ -2,10 +2,10 @@
 namespace tinymeng\tools;
 /**
  * 文件操作类
+ *  logFilePath: /storage/tinymeng/log/
  */
 define('logFilePath',dirname(dirname(dirname(dirname(__DIR__)))).DIRECTORY_SEPARATOR.'storage'.DIRECTORY_SEPARATOR.'tinymeng'.DIRECTORY_SEPARATOR.'log'.DIRECTORY_SEPARATOR);
-class File
-{
+class File{
 
     /**
      * Name: writeLog
@@ -16,7 +16,7 @@ class File
      * @param \Exception|null $exception
      * @return bool
      */
-    static public function writeLog($message, $file_name='error', $echo = false,\Exception $exception = null){
+    static public function writeLog($message, $file_name='error',bool $echo = false,\Exception $exception = null){
         if(!is_string($message)){
             $message = json_encode($message);
         }
@@ -40,14 +40,15 @@ class File
     }
 
     /**
-     * Name: 写入文件
+     * Name: 写入文件(数据小使用file_put_contents)
      * Author: Tinymeng <666@majiameng.com>
-     * @param $file_name 文件名称
-     * @param $content 文件内容
+     * @param string $file_name 文件名称
+     * @param string $content 文件内容
      * @param bool $file_append 内容是否追加
+     * file_put_contents和fopen，fwrite,fclose三个组合的区别
      * http://blog.majiameng.com/article/2724.html
      */
-    static public function filePutContents($file_name,$content,$file_append = false){
+    static public function filePutContents(string $file_name,string $content,bool $file_append = false){
         if(strrpos($file_name,'/')){
             //获取文件夹路径
             $dir_name = substr($file_name,0,strrpos($file_name,'/'));
@@ -55,6 +56,9 @@ class File
             self::mkdir($dir_name);
         }
 
+        if (file_exists($file_name)){
+            chmod($file_name,0755);
+        }
         if($file_append === false){
             file_put_contents($file_name,$content);
         }else{
@@ -66,12 +70,43 @@ class File
     }
 
     /**
+     * Name: 写入文件(文件大，高并发使用fwrite写入)
+     * Author: Tinymeng <666@majiameng.com>
+     * @param string $file_name 文件名称
+     * @param string $content 文件内容
+     * @param bool $file_append 内容是否追加
+     * file_put_contents和fopen，fwrite,fclose三个组合的区别
+     * http://blog.majiameng.com/article/2724.html
+     */
+    static public function fWrite(string $file_name,string $content,bool $file_append = false){
+        if(strrpos($file_name,'/')){
+            //获取文件夹路径
+            $dir_name = substr($file_name,0,strrpos($file_name,'/'));
+            //创建文件夹
+            self::mkdir($dir_name);
+        }
+
+        if (file_exists($file_name)){
+            chmod($file_name,0755);
+        }
+        if($file_append === false){
+            $handle = fopen($file_name, 'w');
+            fwrite($handle, $content);
+            fclose($handle);
+        }else{
+            $handle = fopen($file_name, 'a');
+            fwrite($handle, $content);
+            fclose($handle);
+        }
+    }
+
+    /**
      * Name: 创建文件夹
      * Author: Tinymeng <666@majiameng.com>
      * @param $dir_name
      * @return bool
      */
-    static public function mkdir($dir_name){
+    static public function mkdir(string $dir_name){
         if (!is_dir($dir_name)) {
             if(!mkdir($dir_name, 0755, true)){
                 die('创建缓存文件夹"'.$dir_name.'"失败!');
@@ -86,7 +121,7 @@ class File
      * @param $dir
      * @return bool
      */
-    static public function delDir($dir) {
+    static public function delDir(string $dir) {
         if(!file_exists($dir)){//文件不存在
             return true;
         }
